f00f50cc65d42dafa349b8d4d666128579e93d46
Mohamed Alarbi <54469631+moalarbi@users.noreply.github.com>
2026-04-15 19:30:23 +0300

    Update API_URL in configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxsQkZn8gWAYc1r5jokbWdPS8_foCwEtAeepMJTYGaEqrNdVHRixJ_ijTvhunGkLmRDJw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwNOUh2vdfteTvW0aJcFqqeBcA2JPWBqt3YXkBJQhRxytZt5bvAasHe6FBEUNNt4DO1/exec',
   APP_NAME_AR: 'نظام تسجيل الإيرادات اليومية',
   APP_NAME_EN: 'Daily Revenue Recording System'
 };
